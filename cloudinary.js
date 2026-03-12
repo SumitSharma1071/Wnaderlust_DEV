24821b546c0b27276466ae14908d2ccd6ba274f6
SumitSharma1071 <sumitkumarsharma7294@gmail.com>
2026-03-12 21:21:22 +0530

    Cloudinary Confllict fix

--- cloudinary.js
+++ cloudinary.js
@@ -1,20 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 const multerStorageCloudinary = require("multer-storage-cloudinary");
 
-const CloudinaryStorage = multerStorageCloudinary.CloudinaryStorage;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_SECRET_KEY,
-});
-
-const storage = new CloudinaryStorage({
+const storage = multerStorageCloudinary({
   cloudinary: cloudinary,
-  params: {
-    folder: "wanderlust_DEV",
-    allowed_formats: ["png", "jpg", "jpeg"],
-  },
+  folder: "wanderlust_DEV",
+  allowedFormats: ["png", "jpg", "jpeg"]
 });
 
-module.exports = { cloudinary, storage };
+module.exports = {
+  cloudinary,
+  storage
+};
